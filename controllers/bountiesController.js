c69f72a3162a313b8ba8ab55441391b182cbc758
alexjbustillos <alexjbustillos@gmail.com>
2020-12-15 13:04:29 -0500

    [post]route added need to do put and delete

---
+++ controllers/bountiesController.js
@@ -0,0 +1,30 @@
+const router = require('express').Router()
+
+const models = require('../models')
+
+router.get('/', (req, res) => {
+    models.Bounty.find().then((foundBounties) => {
+        res.status(200).json({ bounties: foundBounties })
+    })
+    .catch(err => { res.send(err) })
+})
+router.get('/:id', (req, res) => {
+    models.Bounty.findOne({_id: req.params.id}).then((bounty) => {
+        res.status(200).json({ bounty })
+    })
+    .catch(err => { res.send(err) })
+})
+router.post('/', (req, res) => {
+    models.Bounty.create(req.body).then((bounty) => {
+        res.status(201).json({ bounty })
+    })
+    .catch(err => { res.send(err) })
+})
+router.put('/:id', (req, res) => {
+    res.send('Hello from PUT /bounties/:id')
+})
+router.delete('/:id', (req, res) => {
+    res.send('Hello from DELETE /bounties/:id')
+})
+
+module.exports = router
--- dbTest.js
+++ dbTest.js
@@ -2,13 +2,20 @@ require('dotenv').config()
 
 const models = require('./models')
 
-// models.Bounty.create({
-//     name: 'Yosemite Sam',
-//     wantedFor: 'Rootin & tootin',
-//     client: 'Walt Disney',
-//     ship: 'good ship lollipop',
-//     hunters: ['Bugs Bunny'],
-//     captured: false,
-//     lastSeen: '1982'
-// })
+models.Bounty.create({
+    name: 'Yosemite Sam',
+    wantedFor: 'Rootin & tootin',
+    client: 'Walt Disney',
+    ship: 'good ship lollipop',
+    reward: 1000,
+    hunters: [{
+        name: 'Bugs Bunny',
+        origin: 'Earth'
+    }],
+    captured: false,
+    lastSeen: '1982'
+})
 
+// models.Bounty.deleteMany().then(() => {
+//     console.log('done!');
+// })
