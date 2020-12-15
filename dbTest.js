f9df340ce494806c947bb4baa894e650f927f202
alexjbustillos <alexjbustillos@gmail.com>
2020-12-15 12:16:37 -0500

    bounties

---
+++ dbTest.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const models = require('./models')
+
+// models.Bounty.create({
+//     name: 'Yosemite Sam',
+//     wantedFor: 'Rootin & tootin',
+//     client: 'Walt Disney',
+//     ship: 'good ship lollipop',
+//     hunters: ['Bugs Bunny'],
+//     captured: false,
+//     lastSeen: '1982'
+// })
+
