246c48fb33665097197f74c95335c5d1badc6ed1
alexjbustillos <alexjbustillos@gmail.com>
2020-12-16 15:50:59 -0500

    backend

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 require('dotenv').config()
 
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const models = require('./models')
 
